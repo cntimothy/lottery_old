b6239fd38c35e8419df8a33c3a3302734cbe747f
cntimothy <cntimothy1990@gmail.com>
2021-01-22 22:55:10 +0800

    enhance:适配2021年版

--- 抽奖/Form1.Designer.cs
+++ 抽奖/Form1.Designer.cs
@@ -79,10 +79,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Gold;
-            this.label1.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(94, 189);
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(94, 280);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.Size = new System.Drawing.Size(0, 38);
             this.label1.TabIndex = 5;
             // 
             // label2
--- 抽奖/Form1.cs
+++ 抽奖/Form1.cs
@@ -199,139 +199,20 @@ namespace 抽奖
             ADt.Columns.Add(ADC5);
             DataRow ADR0 = ADt.NewRow();
             ADR0[0] = 0;
-            ADR0[1] = "阳光普照奖(50元现金红包)";
+            ADR0[1] = "特等奖 vivo Y30";
             ADR0[2] = "pic_0.jpg";
-            ADR0[3] = 10;
-            ADR0[4] = 10;
+            ADR0[3] = 1;
+            ADR0[4] = 3;
             ADt.Rows.Add(ADR0);
 
             DataRow ADR1 = ADt.NewRow();
             ADR1[0] = 1;
-            ADR1[1] = "部门三等奖（小米电水壶）";
+            ADR1[1] = "幸运奖";
             ADR1[2] = "pic_1.jpg";
-            ADR1[3] = 10;
-            ADR1[4] = 1;
+            ADR1[3] = 1;
+            ADR1[4] =30;
             ADt.Rows.Add(ADR1);
 
-            DataRow ADR2 = ADt.NewRow();
-            ADR2[0] = 2;
-            ADR2[1] = "部门三等奖（小米电动牙刷）";
-            ADR2[2] = "pic_2.jpg";
-            ADR2[3] = 10;
-            ADR2[4] = 1;
-            ADt.Rows.Add(ADR2);
-
-            DataRow ADR3 = ADt.NewRow();
-            ADR3[0] = 3;
-            ADR3[1] = "部门三等奖（小米蓝牙项圈耳机）";
-            ADR3[2] = "pic_3.jpg";
-            ADR3[3] = 10;
-            ADR3[4] = 1;
-            ADt.Rows.Add(ADR3);
-
-            DataRow ADR4 = ADt.NewRow();
-            ADR4[0] = 4;
-            ADR4[1] = "部门三等奖（纯棉四件套）";
-            ADR4[2] = "pic_4.jpg";
-            ADR4[3] = 6;
-            ADR4[4] = 2;
-            ADt.Rows.Add(ADR4);
-
-            DataRow ADR5 = ADt.NewRow();
-            ADR5[0] = 5;
-            ADR5[1] = "部门三等奖（拜格锅具套装）";
-            ADR5[2] = "pic_5.jpg";
-            ADR5[3] = 8;
-            ADR5[4] = 1;
-            ADt.Rows.Add(ADR5);
-
-            DataRow ADR6 = ADt.NewRow();
-            ADR6[0] = 6;
-            ADR6[1] = "部门二等奖（扫地机器人）";
-            ADR6[2] = "pic_6.jpg";
-            ADR6[3] = 5;
-            ADR6[4] = 1;
-            ADt.Rows.Add(ADR6);
-
-            DataRow ADR7 = ADt.NewRow();
-            ADR7[0] = 7;
-            ADR7[1] = "部门二等奖（小熊加湿器）";
-            ADR7[2] = "pic_7.jpg";
-            ADR7[3] = 5;
-            ADR7[4] = 1;
-            ADt.Rows.Add(ADR7);
-
-            DataRow ADR8 = ADt.NewRow();
-            ADR8[0] = 8;
-            ADR8[1] = "部门二等奖（小浣熊破壁机）";
-            ADR8[2] = "pic_8.jpg";
-            ADR8[3] = 5;
-            ADR8[4] = 1;
-            ADt.Rows.Add(ADR8);
-
-            DataRow ADR9 = ADt.NewRow();
-            ADR9[0] = 9;
-            ADR9[1] = "部门二等奖（小爱同学智能音箱）";
-            ADR9[2] = "pic_9.jpg";
-            ADR9[3] = 5;
-            ADR9[4] = 1;
-            ADt.Rows.Add(ADR9);
-
-            DataRow ADR10 = ADt.NewRow();
-            ADR10[0] = 10;
-            ADR10[1] = "工会二等奖（美的榨汁机）";
-            ADR10[2] = "pic_10.jpg";
-            ADR10[3] = 10;
-            ADR10[4] = 3;
-            ADt.Rows.Add(ADR10);
-
-            DataRow ADR11 = ADt.NewRow();
-            ADR11[0] = 11;
-            ADR11[1] = "部门一等奖（长虹净化器）";
-            ADR11[2] = "pic_11.jpg";
-            ADR11[3] = 5;
-            ADR11[4] = 1;
-            ADt.Rows.Add(ADR11);
-
-            DataRow ADR12 = ADt.NewRow();
-            ADR12[0] = 12;
-            ADR12[1] = "部门一等奖（倍轻松足疗机）";
-            ADR12[2] = "pic_12.jpg";
-            ADR12[3] = 5;
-            ADR12[4] = 1;
-            ADt.Rows.Add(ADR12);
-
-            DataRow ADR13 = ADt.NewRow();
-            ADR13[0] = 13;
-            ADR13[1] = "工会一等奖（美的压力锅）";
-            ADR13[2] = "pic_13.jpg";
-            ADR13[3] = 10;
-            ADR13[4] = 2;
-            ADt.Rows.Add(ADR13);
-
-            DataRow ADR14 = ADt.NewRow();
-            ADR14[0] = 14;
-            ADR14[1] = "部门特等奖（Vivo Y93手机）";
-            ADR14[2] = "pic_14.jpg";
-            ADR14[3] = 5;
-            ADR14[4] = 1;
-            ADt.Rows.Add(ADR14);
-
-            DataRow ADR15 = ADt.NewRow();
-            ADR15[0] = 15;
-            ADR15[1] = "总经理特别奖（BOE睡眠仪）";
-            ADR15[2] = "pic_15.jpg";
-            ADR15[3] = 5;
-            ADR15[4] = 1;
-            ADt.Rows.Add(ADR15);
-
-            DataRow ADR16 = ADt.NewRow();
-            ADR16[0] = 16;
-            ADR16[1] = "幸运奖";
-            ADR16[2] = "";
-            ADR16[3] = 30;
-            ADR16[4] = 1;
-            ADt.Rows.Add(ADR16);
             //进行绑定  
             comboBox1.DisplayMember = "F_Name";//控件显示的列名  
             comboBox1.ValueMember = "F_ID";//控件值的列名  
